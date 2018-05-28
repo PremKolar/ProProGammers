8dd2bec0afee54c898ee840204f2931736f2500e
Logio <Logio@01>
2018-05-28 10:43:56 +0200

    Test Test Test

---
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/werkzeuge/vormerken/Test.java
@@ -0,0 +1,10 @@
+package de.uni_hamburg.informatik.swt.se2.mediathek.werkzeuge.vormerken;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
